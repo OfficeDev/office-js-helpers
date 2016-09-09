3788437c41b5165c6ed02037c4d9c3e8cc758c1e
Bhargav Krishna <bhkrish@microsoft.com>
2016-09-08 18:47:45 -0700

    commiting messaging for isAuthDialog

--- src/authentication/authenticator.ts
+++ src/authentication/authenticator.ts
@@ -92,13 +92,14 @@ export class Authenticator {
 
     /**
      * Check if the currrent url is running inside of a Dialog that contains an access_token or code or error.
-     * If true then it calls messageParent by extracting the token information.
+     * If true then it calls messageParent by extracting the token information, thereby closing the dialog.
+     * Otherwise, the caller should proceed with normal initialization of their application.
      *
      * @return {boolean}
      * Returns false if the code is running inside of a dialog without the required information
      * or is not running inside of a dialog at all.
      */
-    static closeDialog(): boolean {
+    static isAuthDialog(): boolean {
         if (!Authenticator.isAddin) {
             return false;
         }
