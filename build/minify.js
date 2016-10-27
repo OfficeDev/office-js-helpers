1622d39761c02b253a8f57a12dbb825ddc56a87e
Bhargav Krishna <bhkrish@microsoft.com>
2016-10-27 12:18:11 -0700

    trying out the first build

--- build/minify.js
+++ build/minify.js
@@ -5,8 +5,8 @@ var UglifyJS = require("uglify-js");
 var util = require("util");
 var packageJson = require("../package.json");
 
-var distInFile = "../dist/office.helpers.js";
-var distOutFileUnversioned = "../dist/office.helpers.min.js";
+var distInFile = "./dist/office.helpers.js";
+var distOutFileUnversioned = "./dist/office.helpers.min.js";
 
 var result = UglifyJS.minify(distInFile, { mangle: true });
 fs.writeFileSync(distOutFileUnversioned, result.code, { encoding: "utf-8" });
