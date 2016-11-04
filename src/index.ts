3f7e287e3b12c9ce5031780e83608260ada5e3a3
Bhargav Krishna <bhkrish@microsoft.com>
2016-11-04 11:59:16 -0700

    updating readme and exporting utilities

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license.
 
+export * from './helpers/utilities';
 export * from './helpers/dictionary';
 export * from './helpers/storage';
 export * from './authentication/token.manager';
