27edf30d99206f755142420c5811109ba1869e13
Bhargav Krishna <bhkrish@microsoft.com>
2016-11-03 21:59:53 -0700

    fixing umd output

--- build/browserify.js
+++ build/browserify.js
@@ -5,23 +5,14 @@
 
 var browserify = require("browserify");
 var fs = require("fs");
-var glob = require("glob");
-var mkdirp = require("mkdirp");
-var Umd = require("browserify-umdify");
-var util = require("util");
-
-mkdirp.sync("./temp");
-
-var packageJson = require("../package.json");
 var distOutFileUnversioned = "./dist/office.helpers.js";
 var distOutUnversioned = fs.createWriteStream(distOutFileUnversioned, { encoding: "utf-8", flags: "w" })
 
 var bundled = browserify({
-    extensions: [".js", ".json"],
-    debug: true
+    extensions: [".js"],
+    standalone: 'OfficeHelpers'
 })
     .require("./dist/index.js", { expose: "office-js-helpers" })
-    .bundle()
-    .pipe(new Umd());
+    .bundle();
 
 bundled.pipe(distOutUnversioned);
--- build/minify.js
+++ build/minify.js
@@ -5,9 +5,6 @@
 
 var fs = require("fs");
 var UglifyJS = require("uglify-js");
-var util = require("util");
-var packageJson = require("../package.json");
-
 var distInFile = "./dist/office.helpers.js";
 var distOutFileUnversioned = "./dist/office.helpers.min.js";
 
