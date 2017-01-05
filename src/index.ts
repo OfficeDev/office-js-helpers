815f3e59aa90692fd5ec1d40ede2d94729124235
Bhargav Krishna <bhkrish@microsoft.com>
2017-01-04 21:03:42 -0800

    removing UI

--- src/index.ts
+++ src/index.ts
@@ -10,4 +10,3 @@ export * from './helpers/dialog';
 export * from './authentication/token.manager';
 export * from './authentication/endpoint.manager';
 export * from './authentication/authenticator';
-export * from './ui/ui';
