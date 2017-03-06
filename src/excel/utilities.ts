949e5d1d7489782887730a755c88f64f4d494df6
Bhargav Krishna <bhargav.krishna@outlook.com>
2017-03-06 13:16:10 -0800

    updating method to static and also adding requirements check

--- src/excel/utilities.ts
+++ src/excel/utilities.ts
@@ -9,7 +9,7 @@ export class ExcelUtilities {
      * Utility to delete a worksheet if it already exists.
      * @returns true if the sheet had existed before being deleted.
      */
-    async forceCreateSheet(worksheets: Excel.WorksheetCollection, sheetName: string): Promise<Excel.Worksheet> {
+    static async forceCreateSheet(worksheets: Excel.WorksheetCollection, sheetName: string): Promise<Excel.Worksheet> {
         if (sheetName == null || sheetName.trim() === '') {
             throw new APIError('Sheet name cannot be blank.');
         }
@@ -17,7 +17,10 @@ export class ExcelUtilities {
         const { context } = worksheets;
         const newSheet = (context as Excel.RequestContext).workbook.worksheets.add();
 
-        if (1.1) {
+        if (Office.context.requirements.isSetSupported('ExcelApi', 1.4)) {
+            (context as any).workbook.worksheets.getItemOrNullObject(sheetName).delete();
+        }
+        else {
             // For compatibility with ExcelApi 1.1, using a throwing-behavior check to determine whether the item exists and delete it.
             // In ExcelApi 1.4, this code could instead have been in-lined into the caller by simply doing:
             // context.workbook.worksheets.getItemOrNullObject(sheetName).delete()
@@ -34,12 +37,6 @@ export class ExcelUtilities {
 
                 throw new APIError('Unexpected error while trying to delete sheet.', error);
             }
-        }
-        else {
-            // For compatibility with ExcelApi 1.1, using a throwing-behavior check to determine whether the item exists and delete it.
-            // In ExcelApi 1.4, this code could instead have been in-lined into the caller by simply doing:
-            // context.workbook.worksheets.getItemOrNullObject(sheetName).delete()
-            (context as any).workbook.worksheets.getItemOrNullObject(sheetName).delete();
         }
 
         newSheet.name = sheetName;
