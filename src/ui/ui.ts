8f1353a0fb29112371e18de8e38b8f1074462f88
WrathOfZombies <bhargav.krishna@outlook.com>
2017-10-03 21:26:45 -0700

    streamlining Message Banner

--- src/ui/ui.ts
+++ src/ui/ui.ts
@@ -1,64 +1,61 @@
 /* Copyright (c) Microsoft. All rights reserved. Licensed under the MIT license. */
 
-import { isString, isError, isObject } from 'lodash-es';
-import { CustomError } from '../errors/custom.error';
 import { Utilities, PlatformType } from '../helpers/utilities';
 import html from './message-banner.html';
 
+const DEFAULT_WHITESPACE = 2;
+
+export interface IMessageBannerParams {
+  title?: string;
+  message?: string;
+  type: 'default' | 'success' | 'error' | 'warning' | 'severe-warning';
+  details?: string;
+}
+
 export class UI {
   /** Shows a basic notification at the top of the page
-   * @param message - Message, either single-string or multiline (punctuated by '\n')
-   */
+    * @param message - body of the notification
+    */
   static notify(message: string);
+
+  /** Shows a basic notification with a custom title at the top of the page
+   * @param message - body of the notification
+   * @param title - title of the notification
+   */
+  static notify(message: string, title: string);
+
+  /** Shows a basic notification at the top of the page, with a background color set based on the type parameter
+   * @param message - body of the notification
+   * @param title - title of the notification
+   * @param type - type of the notification - see https://dev.office.com/fabric-js/Components/MessageBar/MessageBar.html#Variants
+   */
+  static notify(message: string, title: string, type: 'default' | 'success' | 'error' | 'warning' | 'severe-warning');
 
   /** Shows a basic error notification at the top of the page
    * @param error - Error object
    */
   static notify(error: Error);
 
-  /** Shows a basic notification with a custom title at the top of the page
-   * @param title - Title, bolded
-   * @param message - Message, either single-string or multiline (punctuated by '\n')
-  */
-  static notify(title: string, message: string);
-
   /** Shows a basic error notification with a custom title at the top of the page
    * @param title - Title, bolded
    * @param error - Error object
    */
-  static notify(title: string, error: Error);
+  static notify(error: Error, title: string);
 
-  /** Shows a basic error notification, with custom parameters, at the top of the page */
-  static notify(error: Error, params: {
-    title?: string;
-    /** custom message in place of the error text */
-    message?: string;
-    moreDetailsLabel?: string;
-  });
+  static notify(...args: any[]) {
+    const params = _parseNotificationParams(args);
+    if (params == null) {
+      console.error(new Error('Invalid params. Cannot create a notification'));
+      return null;
+    }
 
-  /** Shows a basic notification at the top of the page, with a background color set based on the type parameter
-   * @param title - Title, bolded
-   * @param message - Message, either single-string or multiline (punctuated by '\n')
-   * @param type - Type, determines the background color of the notification. Acceptable types are:
-   *               'default' | 'success' | 'error' | 'warning' | 'severe-warning'
-   */
-  static notify(title: string, message: string, type: 'default' | 'success' | 'error' | 'warning' | 'severe-warning');
-
-  /** Shows a basic notification at the top of the page, with custom parameters */
-  static notify(params: {
-    title?: string;
-    message: string;
-    type?: 'default' | 'success' | 'error' | 'warning' | 'severe-warning'
-  });
-
-  static notify() {
-    const params = parseNotificationParams(arguments);
     const messageBarClasses = {
       'success': 'ms-MessageBar--success',
       'error': 'ms-MessageBar--error',
       'warning': 'ms-MessageBar--warning',
       'severe-warning': 'ms-MessageBar--severeWarning'
     };
+
     const messageBarTypeClass = messageBarClasses[params.type] || '';
 
     let paddingForPersonalityMenu = '0';
@@ -95,7 +92,7 @@ export class UI {
       messageTextArea.insertAdjacentElement('beforeend', div);
     });
 
-    if (params.moreDetails) {
+    if (params.details) {
       const labelDiv = document.createElement('div');
       messageTextArea.insertAdjacentElement('beforeend', labelDiv);
       const label = document.createElement('a');
@@ -105,159 +102,55 @@ export class UI {
           .parentElement.style.display = 'block';
         labelDiv.style.display = 'none';
       };
-      label.textContent = params.moreDetailsLabel;
+      label.textContent = 'Details';
       labelDiv.insertAdjacentElement('beforeend', label);
 
       const preDiv = document.createElement('div');
       preDiv.style.display = 'none';
       messageTextArea.insertAdjacentElement('beforeend', preDiv);
       const detailsDiv = document.createElement('pre');
-      detailsDiv.textContent = params.moreDetails;
+      detailsDiv.textContent = params.details;
       preDiv.insertAdjacentElement('beforeend', detailsDiv);
     }
 
     (document.querySelector('.office-js-helpers-notification > button') as HTMLButtonElement)
-      .onclick = () => {
-        notificationDiv.parentNode.removeChild(notificationDiv);
-      };
+      .onclick = () => notificationDiv.parentNode.removeChild(notificationDiv)
   }
 }
 
-function parseNotificationParams(params: IArguments): {
-  title: string;
-  message: string;
-  type: 'default' | 'success' | 'error' | 'warning' | 'severe-warning';
-  moreDetails: string | null;
-  moreDetailsLabel: string;
-} {
-  try {
-    const defaults = {
-      title: null,
-      type: 'default' as ('default' | 'success' | 'error' | 'warning' | 'severe-warning'),
-      moreDetails: null,
-      moreDetailsLabel: 'Additional details...'
-    };
-    switch (params.length) {
-      case 1: {
-        if (isError(params[0])) {
-          return {
-            ...defaults,
-            title: 'Error',
-            type: 'error',
-            ...getErrorDetails(params[0])
-          };
-        }
-        if (isString(params[0])) {
-          return {
-            ...defaults,
-            message: params[0]
-          };
-        }
-        if (isObject(params[0])) {
-          const customParams: {
-            title?: string;
-            message: string;
-            type?: 'default' | 'success' | 'error' | 'warning' | 'severe-warning'
-          } = params[0];
+function _parseNotificationParams(params: any[]): IMessageBannerParams {
+  if (params == null) {
+    return null;
+  }
 
-          if (!isString(customParams.message)) {
-            throw new Error();
-          }
-
-          return {
-            ...defaults,
-            title: customParams.title || defaults.title,
-            message: customParams.message,
-            type: customParams.type || defaults.type,
-          };
-        }
-        throw new Error();
-      }
-
-      case 2: {
-        if (isString(params[0])) {
-          if (isError(params[1])) {
-            return {
-              ...defaults,
-              title: params[0],
-              ...getErrorDetails(params[1])
-            };
-          }
-          if (isString(params[1])) {
-            return {
-              ...defaults,
-              title: params[0],
-              message: params[1]
-            };
-          }
-        }
-        else if (isError(params[0]) && isObject(params[1])) {
-          const customParams: {
-            title?: string;
-            /** custom message in place of the error text */
-            message?: string;
-            moreDetailsLabel?: string;
-          } = params[1];
-
-          const result = {
-            ...defaults,
-            ...getErrorDetails(params[0]),
-            moreDetailsLabel: customParams.moreDetailsLabel || defaults.moreDetailsLabel
-          };
-
-          result.title = customParams.title || result.title;
-          result.message = customParams.message || result.message;
-
-          return result;
-        }
-        throw new Error();
-      }
-
-      case 3: {
-        if (!(isString(params[0]) && isString(params[2]))) {
-          throw new Error();
-        }
-
-        if (!isString(params[1])) {
-          throw new Error();
-        }
-
-        return {
-          ...defaults,
-          title: params[0],
-          message: params[1],
-          type: params[2]
-        };
-      }
-
-      default:
-        throw new Error();
+  const [body, title, type] = params;
+  if (body instanceof Error) {
+    let details = '';
+    const { innerError, stack } = body as any;
+    if (innerError) {
+      let error = JSON.stringify(innerError.debugInfo || innerError, null, DEFAULT_WHITESPACE);
+      details += `Inner Error: \n${error}\n`;
     }
+    if (stack) {
+      details += `Stack Trace: \n${body.stack}\n`;
+    }
+    return {
+      message: body.message,
+      title: title || body.name,
+      type: 'error',
+      details: details
+    };
+  } else if (body instanceof String) {
+    return {
+      message: body as string,
+      title,
+      type: type || 'default',
+      details: null
+    };
+  } else {
+    return {
+      type: 'default',
+      details: JSON.stringify(body, null, DEFAULT_WHITESPACE)
+    };
   }
-  catch (e) {
-    throw new Error('Invalid parameters passed to "notify" function');
-  }
-}
-
-function getErrorDetails(error: Error): {
-  type: 'error'
-  message: string,
-  moreDetails: string;
-} {
-  let moreDetails: string;
-
-  let innerException = error;
-  if (error instanceof CustomError) {
-    innerException = error.innerError;
-  }
-
-  if ((window as any).OfficeExtension && innerException instanceof OfficeExtension.Error) {
-    moreDetails = JSON.stringify((error as OfficeExtension.Error).debugInfo, null, 4);
-  }
-
-  return {
-    type: 'error',
-    message: error.toString(),
-    moreDetails
-  };
 }
