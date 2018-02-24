45bc89aeeb181bcf17d729c757b9eef4532a5f86
WrathOfZombies <bhargav.krishna@outlook.com>
2018-02-24 10:15:46 -0800

    Addressing codacy comments

--- demo/app.js
+++ demo/app.js
@@ -1,11 +1,20 @@
 /// <reference path="../dist/office.helpers.d.ts" />
 
 (function ($) {
-  $('document').ready(function () {
+  // Just a random helper to prettify json
+  function prettify(data) {
+    let json = JSON.stringify(data);
+    json = json.replace(/{/g, "{\n\n\t");
+    json = json.replace(/,/g, ",\n\t");
+    json = json.replace(/}/g, ",\n\n}");
+    return json;
+  }
+
+  $("document").ready(function () {
     // Determine if we are running inside of an authentication dialog
     // If so then just terminate the running function
     if (OfficeHelpers.Authenticator.isAuthDialog()) {
-      // Adding code here isn't guaranteed to run as we need to close the dialog
+      // Adding code here isn"t guaranteed to run as we need to close the dialog
       // Currently we have no realistic way of determining when the dialog is completely
       // closed.
       return;
@@ -15,16 +24,16 @@
     let authenticator = new OfficeHelpers.Authenticator();
 
     // Register our providers accordingly
-    authenticator.endpoints.registerMicrosoftAuth('f59e8034-6e3c-4ba6-9fb5-1342d27d0123');
-    authenticator.endpoints.registerDropboxAuth('tkvf431lh8d9hci');
+    authenticator.endpoints.registerMicrosoftAuth("f59e8034-6e3c-4ba6-9fb5-1342d27d0123");
+    authenticator.endpoints.registerDropboxAuth("tkvf431lh8d9hci");
 
     // Get the output pre
-    let output = $('#output');
+    let output = $("#output");
 
     // Add event handlers to the buttons
-    $('.login').click(function () {
-      let provider = $(this).data('provider');
-      output.text('Authenticating with ' + provider + '...');
+    $(".login").click(function () {
+      let provider = $(this).data("provider");
+      output.text("Authenticating with " + provider + "...");
 
       // Authenticate with the chosen provider
       authenticator.authenticate(provider, true /* setting the force to true, always re-authenticates. This is just for demo purposes */)
@@ -38,13 +47,4 @@
         });
     });
   });
-
-  // Just a random helper to prettify json
-  function prettify(data) {
-    let json = JSON.stringify(data);
-    json = json.replace(/{/g, '{\n\n\t');
-    json = json.replace(/,/g, ',\n\t');
-    json = json.replace(/}/g, ',\n\n}');
-    return json;
-  }
 })(jQuery);
